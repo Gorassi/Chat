ddecbab5b81d21328658ea529598e7a3e6a5a706
Evgeny <efonarev@mail.ru>
2023-07-18 12:45:17 +0300

    third commit

--- client/Client.java
+++ client/Client.java
@@ -1,11 +1,48 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
 
     protected Connection connection;
     private volatile boolean clientConnected = false;
+    
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Enter, please, ip server :");
+        return ConsoleHelper.readString();
+    }
+    
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Enter, please, server port :");
+        return ConsoleHelper.readInt();
+    }
+    
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Enter, please, ip server :");
+         return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){return true;}
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+    
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Didn't send message.");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread{
 
